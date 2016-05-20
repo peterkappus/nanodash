232f5ce7262eab025cd4171e8301b3e5197a494a
Peter Kappus <peterk@peterkappus.com>
2016-05-20 22:16:12 +0100

    remove undefined patch which didn't work.

--- src/js/basicSign.js
+++ src/js/basicSign.js
@@ -2,16 +2,8 @@
 //A simple sign with a few words.
 
 function basic_sign(data) {
+  $("#basic_sign").css('background', data['background']);
+  $("#basic_sign #content").html(data['text']);
 
-  //empty data object is causing issues on my old-ass iPad...
-  //let's try this
-  if(defined(data['text'])){
-    if(defined(data['background'])) {
-      $("#basic_sign").css('background', data['background']);
-    }
-
-    $("#basic_sign #content").html(data['text']);
-
-    $("#basic_sign").fadeIn();
-  }
+  $("#basic_sign").fadeIn();
 }
