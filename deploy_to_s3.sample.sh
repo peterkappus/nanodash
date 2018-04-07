a13b69f86f4d21e55f3f117294d11e92541a6e89
Peter Kappus <peterk@peterkappus.com>
2018-04-07 09:49:00 +0100

    update deploy sample

--- deploy_to_s3.sample.sh
+++ deploy_to_s3.sample.sh
@@ -2,5 +2,6 @@
 #once you've done that, copy this file to something like 'deploy.sh'
 #change "your.bucket.name" to the name of the bucket you want to deploy to
 #then run this everytime you want to upload files to S3
-s3cmd sync dist/ s3://your.bucket.name --delete-removed -P
 
+
+s3cmd sync dist/ s3://your.bucket.name --delete-removed -P --rexclude=.git*
